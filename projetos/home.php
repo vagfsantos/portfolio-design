a5b935f68820f011f2844100f4362663bafff17b
Vagner Santos <santos.vagnerferreira@gmail.com>
2015-10-28 00:51:31 -0200

    Corringindo responsive design home

--- projetos/home.php
+++ projetos/home.php
@@ -1,6 +1,9 @@
 	</head>
 		<body>
-
+		<header class="cabecalho-mobile">
+			<a href="index.php"><img src="imagens/logo.svg" alt="Logotipo Vagner Santos"/></a>
+			<a href="index.php"><h1>vagner<br/>santos</h1></a>
+		</header>
 		<div id="imgBanner"></div>
 		<div id="controladores"><p id="anterior" class="setas">&#xe094</p><p id="proximo" class="setas">&#xe095</p></div>
 		<header id="title-slide">
